24081f5f53b52719b21babdc6ffb6ab6db1b7dd9
drewseffi <drewsutherland23@gmail.com>
2022-02-15 12:27:21 +0000

    Hotfix of App

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -50,7 +50,7 @@ public class App
                 // Wait a bit for db to start
                 Thread.sleep(30000);
                 // Connect to database
-                con = DriverManager.getConnection("jdbc:mysql://db:3306/employees?useSSL=false", "root", "example");
+                con = DriverManager.getConnection("jdbc:mysql://world-db:3306/employees?useSSL=false", "root", "example");
                 System.out.println("Successfully connected");
                 break;
             }
