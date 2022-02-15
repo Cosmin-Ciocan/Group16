11ef55eebe93b77c2a49be23b7cc0478ea25e490
drewseffi <drewsutherland23@gmail.com>
2022-02-15 11:42:41 +0000

    SQL query testing

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -11,11 +11,15 @@ public class App
 
         // Connect to database
         a.connect();
+        // Get Employee
+        Employee emp = a.getEmployee(255530);
+        // Display results
+        a.displayEmployee(emp);
 
         // Disconnect from database
         a.disconnect();
     }
-    
+
     /**
      * Connection to MySQL database.
      */
@@ -78,6 +82,55 @@ public class App
             {
                 System.out.println("Error closing connection to database");
             }
+        }
+    }
+
+    public Employee getEmployee(int ID)
+    {
+        try
+        {
+            // Create an SQL statement
+            Statement stmt = con.createStatement();
+            // Create string for SQL statement
+            String strSelect =
+                    "SELECT emp_no, first_name, last_name "
+                            + "FROM employees "
+                            + "WHERE emp_no = " + ID;
+            // Execute SQL statement
+            ResultSet rset = stmt.executeQuery(strSelect);
+            // Return new employee if valid.
+            // Check one is returned
+            if (rset.next())
+            {
+                Employee emp = new Employee();
+                emp.emp_no = rset.getInt("emp_no");
+                emp.first_name = rset.getString("first_name");
+                emp.last_name = rset.getString("last_name");
+                return emp;
+            }
+            else
+                return null;
+        }
+        catch (Exception e)
+        {
+            System.out.println(e.getMessage());
+            System.out.println("Failed to get employee details");
+            return null;
+        }
+    }
+
+    public void displayEmployee(Employee emp)
+    {
+        if (emp != null)
+        {
+            System.out.println(
+                    emp.emp_no + " "
+                            + emp.first_name + " "
+                            + emp.last_name + "\n"
+                            + emp.title + "\n"
+                            + "Salary:" + emp.salary + "\n"
+                            + emp.dept_name + "\n"
+                            + "Manager: " + emp.manager + "\n");
         }
     }
 }
---
+++ src/main/java/com/napier/sem/Employee.java
@@ -0,0 +1,42 @@
+package com.napier.sem;
+
+/**
+ * Represents an employee
+ */
+public class Employee
+{
+    /**
+     * Employee number
+     */
+    public int emp_no;
+
+    /**
+     * Employee's first name
+     */
+    public String first_name;
+
+    /**
+     * Employee's last name
+     */
+    public String last_name;
+
+    /**
+     * Employee's job title
+     */
+    public String title;
+
+    /**
+     * Employee's salary
+     */
+    public int salary;
+
+    /**
+     * Employee's current department
+     */
+    public String dept_name;
+
+    /**
+     * Employee's manager
+     */
+    public String manager;
+}
