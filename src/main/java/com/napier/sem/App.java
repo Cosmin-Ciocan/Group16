92a8617a603d44219c6583cadf28e3274b1bee88
MathiasM-Git <80817933+MathiasM-Git@users.noreply.github.com>
2022-02-17 14:58:41 +0000

    Update App

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -1,31 +1,26 @@
 package com.napier.sem;
 
 import java.sql.*;
+import java.util.ArrayList;
 
-public class App
-{
-    public static void main(String[] args)
-    {
+public class App {
+    public static void main(String[] args) {
         // Create new Application
         App a = new App();
 
         // Connect to database
         a.connect();
-        // Get Employee
-        Country emp = a.getCountry(300000);
-        // Display results
-        a.displayCountry(emp);
+
+        a.printCountries(a.test());
 
         // Disconnect from database
         a.disconnect();
     }
 
-    public void displayCountry(Country country)
-    {
-        if (country != null)
-        {
+    public void displayCountry(Country country) {
+        if (country != null) {
             System.out.println(
-                    country.test2 + "\n");
+                    country.name + "\n");
         }
     }
 
@@ -37,39 +32,29 @@ public class App
     /**
      * Connect to the MySQL database.
      */
-    public void connect()
-    {
-        try
-        {
+    public void connect() {
+        try {
             // Load Database driver
             Class.forName("com.mysql.cj.jdbc.Driver");
-        }
-        catch (ClassNotFoundException e)
-        {
+        } catch (ClassNotFoundException e) {
             System.out.println("Could not load SQL driver");
             System.exit(-1);
         }
 
         int retries = 10;
-        for (int i = 0; i < retries; ++i)
-        {
+        for (int i = 0; i < retries; ++i) {
             System.out.println("Connecting to database...");
-            try
-            {
+            try {
                 // Wait a bit for db to start
                 Thread.sleep(30000);
                 // Connect to database
                 con = DriverManager.getConnection("jdbc:mysql://db:3306/world?useSSL=false", "root", "example");
                 System.out.println("Successfully connected");
                 break;
-            }
-            catch (SQLException sqle)
-            {
+            } catch (SQLException sqle) {
                 System.out.println("Failed to connect to database attempt " + Integer.toString(i));
                 System.out.println(sqle.getMessage());
-            }
-            catch (InterruptedException ie)
-            {
+            } catch (InterruptedException ie) {
                 System.out.println("Thread interrupted? Should not happen.");
             }
         }
@@ -78,26 +63,19 @@ public class App
     /**
      * Disconnect from the MySQL database.
      */
-    public void disconnect()
-    {
-        if (con != null)
-        {
-            try
-            {
+    public void disconnect() {
+        if (con != null) {
+            try {
                 // Close connection
                 con.close();
-            }
-            catch (Exception e)
-            {
+            } catch (Exception e) {
                 System.out.println("Error closing connection to database");
             }
         }
     }
 
-    public Country getCountry(int pop)
-    {
-        try
-        {
+    public Country getCountry(int pop) {
+        try {
             // Create an SQL statement
             Statement stmt = con.createStatement();
             // Create string for SQL statement
@@ -109,20 +87,57 @@ public class App
             ResultSet rset = stmt.executeQuery(strSelect);
             // Return new employee if valid.
             // Check one is returned
-            if (rset.next())
-            {
+            if (rset.next()) {
                 Country country = new Country();
-                country.test2 = rset.getString("Name");
+                country.name = rset.getString("Name");
                 return country;
-            }
-            else
+            } else
                 return null;
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            System.out.println("Failed to get details");
+            return null;
+        }
+    }
+
+    // WORLD //
+
+    public ArrayList<Country> test() {
+        try
+        {
+            Statement stmt = con.createStatement();
+
+            String strSelect = "SELECT Name FROM `country` ORDER BY Population DESC";
+
+            ResultSet resultSet = stmt.executeQuery(strSelect);
+
+            ArrayList<Country> countries = new ArrayList<Country>();
+            while (resultSet.next())
+            {
+                Country cty = new Country();
+                cty.name = resultSet.getString("Name");
+                countries.add(cty);
+            }
+            return countries;
         }
         catch (Exception e)
         {
             System.out.println(e.getMessage());
-            System.out.println("Failed to get employee details");
+            System.out.println("Failed to get countries list");
             return null;
+        }
+    }
+
+
+    public void printCountries(ArrayList<Country> countries)
+    {
+        //print header
+        System.out.println(String.format("%-10s", "Name"));
+        // Loop over all countries in the list
+        for (Country cty  : countries)
+        {
+            String emp_string = String.format("%-10s",cty.name);
+            System.out.println(emp_string);
         }
     }
 }
--- src/main/java/com/napier/sem/Country.java
+++ src/main/java/com/napier/sem/Country.java
@@ -1,21 +1,68 @@
 package com.napier.sem;
 
+/**
+ * Represents a country
+ */
 public class Country
 {
-    public String test1;
-    public String test2;
-    public String test3;
-    public String test4;
-    public String test5;
-    public Double test6;
-    public Integer test7;
-    public Integer test8;
-    public Double test9;
-    public Double test10;
-    public Double test11;
-    public String test12;
-    public String test13;
-    public String test14;
-    public Integer test15;
-    public String test16;
+    /**
+     * Country code
+     */
+    public String code;
+    /**
+     * Country name
+     */
+    public String name;
+    /**
+     * Country's continent
+     */
+    public String continent;
+    /**
+     * Country's region
+     */
+    public String region;
+    /**
+     * Country's surface area
+     */
+    public Float surfaceArea;
+    /**
+     * Country's year of independence (can be null)
+     */
+    public Integer indepYear;
+    /**
+     * Country's population
+     */
+    public Integer population;
+    /**
+     * Country's average life expectancy
+     */
+    public Float lifeExpectancy;
+    /**
+     * Country's gross national product
+     */
+    public Float GNP;
+    /**
+     * Country's old gross national product (can be null)
+     */
+    public Float GNPOld;
+    /**
+     * Country's local name
+     */
+    public String localName;
+    /**
+     * Country's government form
+     */
+    public String GovernmentForm;
+    /**
+     * Country's head of state name
+     */
+    public String headOfState;
+    /**
+     * Country's capital city name
+     */
+    public Integer capital;
+    /**
+     * Country's code 2
+     */
+    public String code2;
 }
