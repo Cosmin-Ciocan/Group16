90a639a2d4230c65055b51d95c5f3cd2ef58ed9e
AgatheMullot <40547459@live.napier.ac.uk>
2022-03-02 17:32:11 +0000

    SQL function topCitiesWorldPop

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -17,7 +17,7 @@ public class App
         // Connect to database
         a.connect();
 
-        a.cityDistrictPop();
+        a.topCitiesWorldPop();
 
         // Disconnect from database
         a.disconnect();
@@ -195,7 +195,42 @@ public class App
         }
     }
 
+    /**
+     * The top N populated cities in the world where N is provided by the user.
+     */
+    public void topCitiesWorldPop() {
+        try
+        {
+            Statement stmt = con.createStatement();
 
+            String strSelect = "SELECT Name,Population FROM city ORDER BY Population DESC LIMIT 10";
+
+            ResultSet resultSet = stmt.executeQuery(strSelect);
+
+            ArrayList<City> cities = new ArrayList<>();
+            while (resultSet.next())
+            {
+                City city = new City();
+                city.name = resultSet.getString("Name");
+                city.population = resultSet.getInt("Population");
+                cities.add(city);
+            }
+
+            //print header
+            System.out.println(String.format("|%-10s|%-10s|", "Name", "Population"));
+            // Loop over all countries in the list
+            for (City city : cities)
+            {
+                String cty_string = String.format("|%-10s|%-10s|",city.name, city.population);
+                System.out.println(cty_string);
+            }
+        }
+        catch (Exception e)
+        {
+            System.out.println(e.getMessage());
+            System.out.println("Failed to get countries list");
+        }
+    }
 
     //CONTINENT//
     /**
